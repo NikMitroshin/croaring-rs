af331c7c2f197160dcf3b09c208ee3c735b42dab
nikitamitroshin <mitroshin.js@gmail.com>
2024-02-26 21:21:03 +0700

    fix build

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -10,24 +10,16 @@ fn main() {
         .file("CRoaring/roaring.c")
         .compile("roaring");
 
-        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    #[cfg(feature = "buildtime_bindgen")]
-    {
     let bindings = bindgen::Builder::default()
         .blacklist_type("max_align_t")
         .header("CRoaring/roaring.h")
         .generate_inline_functions(true)
         .generate()
         .expect("Unable to generate bindings");
-    }
-        #[cfg(not(feature = "buildtime_bindgen"))]
-           {
-               use std::fs;
-               fs::copy(
-                   "CRoaring/bindgen_bundled_version.rs",
-                   out_path.join("croaring-sys.rs"),
-               )
-               .expect("Could not copy bindings to output directory");
-           }
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("croaring-sys.rs"))
+        .expect("Couldn't write bindings!");
 }
 
